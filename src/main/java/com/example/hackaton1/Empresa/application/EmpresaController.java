0ff0aa5164885857765b4a60f75739e5448b025a
Adrian <adrian.poma@utec.edu.pe>
2025-05-06 17:01:01 -0500

    empresa

---
+++ src/main/java/com/example/hackaton1/Empresa/application/EmpresaController.java
@@ -0,0 +1,4 @@
+package com.example.hackaton1.Empresa.application;
+
+public class EmpresaController {
+}
---
+++ src/main/java/com/example/hackaton1/Empresa/infrastructure/EmpresaRepository.java
@@ -0,0 +1,4 @@
+package com.example.hackaton1.Empresa.infrastructure;
+
+public interface EmpresaRepository {
+}
